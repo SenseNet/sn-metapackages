6ced1cac25e65e457cc4438aaac84f192e0ae060
tusmester <tusmester@gmail.com>
2020-11-11 09:50:37 +0100

    Add documentation to UseSenseNet methods.

--- src/SenseNet.Services.Sql.SearchService/SenseNetExtensions.cs
+++ src/SenseNet.Services.Sql.SearchService/SenseNetExtensions.cs
@@ -48,6 +48,18 @@ namespace SenseNet.Extensions.DependencyInjection
             return services;
         }
 
+        /// <summary>
+        /// Registers sensenet middlewares.
+        /// If you want to inject a middleware after the user was authenticated, use the onAfterAuthentication method parameter. 
+        /// </summary>
+        /// <remarks>
+        /// Please note that some of the middlewares (e.g. OData) are branching the pipeline. That means you cannot register
+        /// a custom middleware that runs after them. To be able to that, you have to call the Use methods in this method
+        /// directly and specify an app builder branch there.
+        /// </remarks>
+        /// <param name="app">The application builder instance.</param>
+        /// <param name="onAfterAuthentication">An app builder method that can be used to register middlewares after
+        /// authentication but before the main sensenet middlewares (e.g. OData).</param>
         public static IApplicationBuilder UseSenseNet(this IApplicationBuilder app, Action<IApplicationBuilder> onAfterAuthentication = null)
         {
             // custom CORS policy
